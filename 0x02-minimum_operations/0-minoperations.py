b7933894d3ea72a19ec02a0b6af87a45c90ae0e9
BrianEtemesi <etemecci@gmail.com>
2023-07-13 19:22:05 +0000

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+calculates the fewest number of operations needed
+to result in exactly n H characters in the file.
+"""
+
+
+def get_factor(n):
+    """
+    get sum of prime factors of n or n if n is prime
+    """
+    i = 2
+    j = 0
+
+    while i <= n:
+        if n % i == 0:
+            n = n // i
+            j += i
+            i -= 1
+        i += 1
+    return j
+
+
+def minOperations(n):
+    """
+    returns the fewest number of operations needed
+    """
+    
+    if not isinstance(n, int) or n < 2:
+        return 0
+    return get_factor(n)
