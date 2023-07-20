5db1528b9e482cec33205a08e5544479dda8b1f5
BrianEtemesi <etemecci@gmail.com>
2023-07-20 12:22:28 +0000

    change code format

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,42 +4,33 @@ reads from stdin line by line and computes metrics
 """
 import sys
 
+line_count = 0
+FileSize = 0
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
+          '403': 0, '404': 0, '405': 0, '500': 0}
+codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+try:
+    for line in sys.stdin:
+        line_count += 1
 
-def parse_log():
-    """
-    parses log files and prints out statistics
-    """
-    total_size = 0
-    status_code_counts = {}
+        # Parse the line and extract relevant information
+        parts = line.split(' ')
 
-    try:
-        line_count = 0
-        for line in sys.stdin:
-            line_count += 1
-            if line_count % 10 == 0:
-                print_statistics(total_size, status_code_counts)
+        if len(parts) > 2:
+            FileSize += int(parts[-1])
+            if parts[-2] in status:
+                status[parts[-2]] += 1
 
-            # Parse the line and extract relevant information
-            parts = line.split()
-            if len(parts) >= 9 and parts[7].isdigit():
-                status_code = int(parts[7])
-                file_size = int(parts[8])
-                total_size += file_size
+        if line_count % 10 == 0:
+            print("File size: {}".format(FileSize))
+            for code in codes:
+                if status[code]:
+                    print("{}: {}".format(code, status[code]))
 
-                if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
-                    x = status_code_counts.get(status_code, 0) + 1
-                    status_code_counts[status_code] = x
-
-    except KeyboardInterrupt:
-        # If the user interrupts with CTRL+C, print the final statistics
-        print_statistics(total_size, status_code_counts)
-
-
-def print_statistics(total_size, status_code_counts):
-    print("File size: {}".format(total_size))
-    for status_code in sorted(status_code_counts.keys()):
-        print("{}: {}".format(status_code, status_code_counts[status_code]))
-
-
-if __name__ == "__main__":
-    parse_log()
+except KeyboardInterrupt:
+    pass
+finally:
+    print("File size: {}".format(FileSize))
+    for code in codes:
+        if status[code]:
+            print("{}: {}".format(code, status[code]))
