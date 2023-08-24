e9ab74eb53b84a0146f846b53b36590094f8651f
BrianEtemesi <etemecci@gmail.com>
2023-08-24 21:32:03 +0000

    pycode error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,20 +10,20 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
+
     # Sort the coin denominations in descending order
     coins.sort(reverse=True)
-    
+
     num_coins = 0
     remaining = total
-    
+
     # Iterate through each coin denomination
     for coin in coins:
-        # While the remaining amount is greater than or equal to the current coin
+        # While the remaining amount is greater than/equal to the current coin
         while remaining >= coin:
-            remaining -= coin  # Subtract the coin value from the remaining amount
+            remaining -= coin  # Subtract coin value from the remaining amount
             num_coins += 1     # Increment the count of used coins
-    
+
     # If the remaining amount is 0, return the number of coins used
     if remaining == 0:
         return num_coins
